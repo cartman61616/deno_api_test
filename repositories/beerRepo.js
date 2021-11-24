7daf296f0347803424ed8f29a3cff1c0094789a7
Jonathan Davis <cartman61616@gmail.com>
2021-11-24 10:34:46 -0500

    Finish repo and service

--- repositories/beerRepo.js
+++ repositories/beerRepo.js
@@ -14,4 +14,35 @@ class BeerRepo {
     selectAll() {
         return client.queryArray("SELECT * FROM beers ORDER BY id");
     }
+
+    selectById(id) {
+        return client.queryObject(`SELECT *
+                                   FROM beers
+                                   WHERE id = ${id}`, id);
+    }
+
+    update(id, beer) {
+        let latestBeer = this.selectById(id);
+        let query = `UPDATE beers
+                     SET name       = $1,
+                         brand      = $2,
+                         is_premium = $3
+                     WHERE id = $4`;
+
+        return client.queryObject(
+            query,
+            beer.name !== undefined ? beer.name : latestBeer.name,
+            beer.brand !== undefined ? beer.brand : latestBeer.brand,
+            beer.is_premium !== undefined ? beer.is_premium : latestBeer.is_premium,
+            id
+        );
+    }
+
+    delete(id) {
+        return client.queryObject(`DELETE
+                                   FROM beers
+                                   WHERE id = $1`, id);
+    }
 }
+
+export default new BeerRepo();
--- services/beerService.js
+++ services/beerService.js
@@ -16,3 +16,50 @@ export const getBeers = async () => {
 
     return result;
 };
+
+export const getBeer = async (beerId) => {
+    const beers = await beerRepo.selectById(beerId);
+
+    let result = {};
+    beers.rows.map((beer) => {
+        result = beer;
+    });
+
+    return result;
+}
+
+export const createBeer = async (beerData) => {
+    const newBeer = {
+        name: String(beerData.name),
+        brand: String(beerData.brand),
+        is_premium: "is_premium" in beerData ? Boolean(beerData.is_premium) : false,
+        registration_date: new Date(),
+    };
+
+    await beerRepo.create(newBeer);
+
+    return newBeer.id;
+}
+
+export const updateBeer = async (beerId, beerData) => {
+    const beer = await getBeer(beerId);
+
+    if (Object.keys(beer).length === 0 && beer.constructor === Object) {
+        throw new Error("Beer not found");
+    }
+
+    const updatedBeer = {
+        name: beerData.name !== undefined ? String(beerData.name) : beer.name,
+        brand: beerData.brand !== undefined ? String(beerData.brand) : beer.brand,
+        is_premium:
+            beerData.is_premium !== undefined
+                ? Boolean(beerData.is_premium)
+                : beer.is_premium,
+    };
+
+    beerRepo.update(beerId, updatedBeer);
+};
+
+export const deleteBeer = async (beerId) => {
+    beerRepo.delete(beerId);
+};
